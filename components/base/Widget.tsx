55ed588d37b8ab187609b3b6e211e3a337893807
Jeff Son <jeff@infograb.net>
2024-04-23 01:41:34 +0900

    feat: drag & drop 색 조정

--- components/base/Widget.tsx
+++ components/base/Widget.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useRef, useState } from "react";
 interface WidgetProps {
   children: React.ReactNode;
   isDragging: boolean;
-  onDrop?: (file: File) => void;
+  onDrop?: (files: FileList) => void;
 }
 
 export default function Widget({ children, isDragging, onDrop }: WidgetProps) {
@@ -12,8 +12,6 @@ export default function Widget({ children, isDragging, onDrop }: WidgetProps) {
     <article
       className="flex aspect-video w-72 items-center justify-center rounded-lg bg-gray-100 relative overflow-hidden"
       onDragOver={(e) => {
-        console.log("drag over");
-
         e.preventDefault();
         setIsDraggingOver(true);
       }}
@@ -23,15 +21,26 @@ export default function Widget({ children, isDragging, onDrop }: WidgetProps) {
       onDrop={(e) => {
         if (onDrop === undefined) return;
         e.preventDefault();
-        onDrop(e.dataTransfer.files[0]);
+        onDrop(e.dataTransfer.files);
       }}
     >
-      {isDragging && (
+      {onDrop && isDragging && (
         <div
-          className={`${isDraggingOver ? "bg-gray-400" : "bg-gray-100 bg-opacity-80"}
-          absolute inset-0  flex items-center justify-center`}
+          className={`${isDraggingOver ? "bg-gray-400" : "bg-gray-200 bg-opacity-90"}
+          absolute inset-0 items-center justify-center`}
         >
-          <p className="text-gray-500">Drop here</p>
+          {/* Dotted rectangle */}
+          <div
+            className={`${isDraggingOver ? "border-gray-200" : "border-gray-400"}
+          absolute inset-0 m-3 border-dashed border-2 rounded-lg `}
+          >
+            {/* add "Drop here" text in the center of the rectangle */}
+            <div className="absolute inset-0 flex items-center justify-center text-2xl">
+              <p className={isDraggingOver ? "text-gray-200" : "text-gray-400"}>
+                Drop here
+              </p>
+            </div>
+          </div>
         </div>
       )}
       <div className="w-9/12 flex h-full flex-col items-center justify-center overflow-hidden">
