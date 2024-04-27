7ada57dd5a85aac7298bdf852cdb5a65f014114b
Jeff Son <jeff@infograb.net>
2024-04-27 15:11:17 +0900

    feat: add EncodeWidget

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import EncodeWidget from "@/components/widget/Encode/EncodeWidget";
 import CrontabWidget from "@/components/widget/cron/CronWidget";
 import InspirationalQuotesWidget from "@/components/widget/inspirational_quotes/InspirationalQuotesWidget";
 import JsonWidget from "@/components/widget/json/JsonWidget";
@@ -24,6 +25,7 @@ export default function Home() {
     >
       <div className="w-full min-w-72 p-0 sm:p-2 mt-5">
         <div className="flex flex-wrap gap-x-5 gap-y-5 justify-center">
+          <EncodeWidget />
           <InspirationalQuotesWidget />
           <JsonWidget isDragging={isDragging} />
           <CrontabWidget />
