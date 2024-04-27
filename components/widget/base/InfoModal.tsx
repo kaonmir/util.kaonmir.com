2e557bdbc1a107ec27e02c3fdcc6eba5beaf7718
Jeff Son <jeff@infograb.net>
2024-04-27 12:15:07 +0900

    feat: add responsive info modal

--- components/widget/base/InfoModal.tsx
+++ components/widget/base/InfoModal.tsx
@@ -1,3 +1,4 @@
+import useWindowSize from "@/components/hook/useWindowSize";
 import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useRef, useState } from "react";
@@ -11,6 +12,12 @@ export default function InfoModal({ description }: InfoModalProps) {
   const infoRef = useRef<HTMLDivElement>(null);
 
   const [isOpened, setIsOpened] = useState<boolean>(false);
+  const [isLeft, setIsLeft] = useState<boolean>(false);
+
+  useEffect(() => {
+    window.addEventListener("resize", onResize);
+    return () => document.removeEventListener("resize", onResize);
+  });
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -34,10 +41,20 @@ export default function InfoModal({ description }: InfoModalProps) {
     };
   }, [isOpened]);
 
+  const onResize = () => {
+    const widgetRight = infoRef.current?.getBoundingClientRect().right;
+    if (!widgetRight) return;
+
+    const modalRight = widgetRight + 180; //! Size Hard Coded
+    if (modalRight >= window.innerWidth) setIsLeft(true);
+    else setIsLeft(false);
+  };
+
   return (
     <div>
       <div
         ref={infoRef}
+        className="absolute top-0 right-0 p-2 z-10 clickable text-gray-300 hover:text-gray-500"
         onClick={() => setIsOpened(!isOpened)}
       >
         <FontAwesomeIcon icon={faInfoCircle} />
@@ -45,6 +62,10 @@ export default function InfoModal({ description }: InfoModalProps) {
       <div
         ref={modalRef}
         className={`
+          ${isOpened ? "block" : "hidden"}
+          ${isLeft ? "mr-7" : "-mr-[10.5rem]"}
+          absolute top-2 right-0 w-40  bg-white rounded-lg
+          z-10 p-2 shadow-lg`}
       >
         <p className="text-sm text-gray-600 leading-4">{description}</p>
       </div>
